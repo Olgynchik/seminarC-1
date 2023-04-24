d6c47642d2aa9e298a308d7d638081c3abf10f02
Olga <sinickaya1986@gmail.com>
2023-04-24 18:24:52 +0300

    task_01

---
+++ seminar01/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// написать программу, которая на вход принимает число и выдает его квадрат
+// (число умноженное само на себя)
+//  4->16
+// -3->9
+//  -7->49
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"квадратное число: {sqr}");
