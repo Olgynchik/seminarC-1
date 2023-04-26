0d5b7cc4d0972d9fe89ba8b86d67b076ef5862ac
Olga <sinickaya1986@gmail.com>
2023-04-26 10:29:50 +0300

    Пример решения

---
+++ seminar01/task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// напишите программу,  которая принимает на вход трехзначное число
+// и на выход показывает последнюю цифру этого числа 
+System.Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+if(A > 99 && A < 1000)
+{
+    System.Console.WriteLine(A % 10);
+}
+System.Console.WriteLine("Error");
